009e33d5bbbab903e9bcb4e17465dc47caa8f69a
Anup Gupta <anupg5264@gmail.com>
2026-02-14 14:08:51 +0000

    plzzzz understand on the calender system

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
-import { DayPicker, SelectSingleEventHandler } from "react-day-picker"
+import { DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -34,9 +34,11 @@ function Calendar({
         nav_button_previous: "absolute left-1",
         nav_button_next: "absolute right-1",
         table: "w-full border-collapse space-y-1",
+        head_row: "flex",
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
-        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        row: "flex w-full mt-2",
+        cell: "h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected])]:bg-accent focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "h-9 w-9 p-0 font-normal aria-selected:opacity-100"
@@ -64,7 +66,7 @@ function Calendar({
             <button
               type="button"
               onClick={() => {
-                const onSelectHandler = props.onSelect as (value: Date) => void;
+                const onSelectHandler = props.onSelect as (date?: Date) => void;
                 onSelectHandler(today);
               }}
               className={cn(buttonVariants({ variant: "link" }), "text-sm text-muted-foreground")}
@@ -76,7 +78,7 @@ function Calendar({
       }
       {...props}
     />
-  );
+  )
 }
 Calendar.displayName = "Calendar"
 
